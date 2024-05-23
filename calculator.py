d6d4a64a99519c2b8adfc4ed021111d5694bdc1f
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-05-23 16:04:36 +0300

    app is ready

--- calculator.py
+++ calculator.py
@@ -1,25 +1,31 @@
-try:
-    num1 = float(input("Enter first number: "))
-    operation = input("Enter operation: ")
-    num2 = float(input("Enter second number: "))
+def calculator(operation,num1,num2):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 == 0:
+            raise ValueError("Division by zero is not possible")
+        return num1 / num2
+    else:
+        raise ValueError("Unsupported operation")
 
-    def calculator(operation,num1,num2):
-        if operation == '+':
-            return num1 + num2
-        elif operation == '-':
-            return num1 - num2
-        elif operation == '*':
-            return num1 * num2
-        elif operation == '/':
-            if num2 == 0:
-                raise ValueError("Division by zero is not possible")
-            return num1 / num2
-        else:
-            raise ValueError("Unsupported operation")
+def main():
+    try:
+        num1 = float(input("Enter first number: "))
+        operation = input("Enter operation: ")
+        num2 = float(input("Enter second number: "))
 
-    print(calculator(operation, num1, num2))
-except ValueError as e:
-    print("Error:", e)
+        result = calculator(operation, num1, num2)
+        print("The result is:", result)
+
+    except ValueError as e:
+        print("Error:", e) 
+
+if __name__ == "__main__":
+    main()           
 
 
     
